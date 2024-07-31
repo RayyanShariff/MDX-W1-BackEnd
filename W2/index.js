3485b97a8c7c40b2329062952fbaf77849fe93f6
RayyanShariff <64745334+RayyanShariff@users.noreply.github.com>
2024-08-01 00:48:18 +0400

    Update index.js and added comments

--- W2/index.js
+++ W2/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); //For reading the input from the form
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
@@ -13,16 +13,19 @@ app.post('/', (req, res) => {
     var result
     if (req.body.currency === 'usd') {
         result = amount * 0.27;
+        res.send("Converted amount is: " + result + " USD");
     } 
     else if (req.body.currency === 'eur') {
         result = amount * 0.25;
+        res.send("Converted amount is: " + result + " EUR");
     } 
     else if (req.body.currency === 'omr') {
         result = amount * 0.10;
+        res.send("Converted amount is: " + result + " OMR");
     }
-    res.send("Converted amount is: " + result);
     });
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
+// To run the file and display it on the browser, use the following command: node index.js
